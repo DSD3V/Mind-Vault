64764c0b346ec76f4afd4140910ef3d09443203d
DS <>
2022-12-18 19:09:30 -0600

    updated local server address

--- backend/server.js
+++ backend/server.js
@@ -26,5 +26,5 @@ const driver = neo4j.driver(
 );
 export const session = driver.session();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}.`));
